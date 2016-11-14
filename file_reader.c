745ea26641bbf0d5f4e63d4c5ba86b3a06557d89
Alessandro Marzialetti <setti93@gmail.com>
2016-11-14 14:41:42 +0100

    aa

--- file_reader.c
+++ file_reader.c
@@ -6,18 +6,59 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 10:13:57 by amarzial          #+#    #+#             */
-/*   Updated: 2016/11/14 10:44:52 by amarzial         ###   ########.fr       */
+/*   Updated: 2016/11/14 14:41:25 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fillit.h>
+#include "libft.h"
+#include "fillit.h"
+#include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
-int		is_valid(char *tile, int size)
+int		isvalid(char *tile, int size)
 {
+	int		cnt;
+	int		blocks;
+
+	cnt = 0;
+	blocks = 0;
 	if (size != TILE_SIZE)
 		return (0);
-	
+	while (cnt < TILE_SIZE)
+	{
+		if ((cnt + 1) % 5)
+		{
+			if (tile[cnt] == '.' || (tile[cnt] == '#' && blocks < 4))
+				++blocks;
+			else
+				return (0);
+		}
+		else
+			if (tile[cnt] != '\n')
+				return (0);
+		++cnt;
+	}
+	return (1);
+}
+
+int		convert(t_tile *dest, char *tile, int size)
+{
+	t_point	p;
+	t_tile	ntl;
+	int		idx;
+
+	p.y = 0;
+	idx = 0;
+	if (!isvalid(tile, size) || (ntl = (t_tile*(ft_memalloc(sizeof(t_tile))))))
+		return (0);
+	while (p.y < 4)
+	{
+		p.x = 0;
+		while (p.x < 4)
+			if (tile[(p.y * 5) + p.x] == '#')
+				newtile.dots[idx] = p;
+	}
 }
 
 int		read_tiles(int fd)
