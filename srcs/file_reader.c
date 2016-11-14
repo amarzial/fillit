803d82174b14d4148e70010214beda9f380a86d4
Alessandro Marzialetti <setti93@gmail.com>
2016-11-14 17:21:41 +0100

    better

--- srcs/file_reader.c
+++ srcs/file_reader.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 10:13:57 by amarzial          #+#    #+#             */
-/*   Updated: 2016/11/14 14:41:25 by amarzial         ###   ########.fr       */
+/*   Updated: 2016/11/14 17:20:43 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-int		isvalid(char *tile, int size)
+static int		isvalid(char *tile, int size)
 {
 	int		cnt;
 	int		blocks;
@@ -42,26 +42,44 @@ int		isvalid(char *tile, int size)
 	return (1);
 }
 
-int		convert(t_tile *dest, char *tile, int size)
+static int		hascontact(const t_point *p1, const t_point *p2)
+{
+	if (p1->x == p2->x && abs(p1->y - p2->y) == 1)
+		return (1);
+	if (p1->y == p2->y && abs(p1->x - p2->x) == 1)
+		return (1);
+	return (0);
+}
+
+static t_tile	*convert(t_tile *dest, char *tile, int size)
 {
 	t_point	p;
-	t_tile	ntl;
+	t_tile	*ntl;
 	int		idx;
+	int		tmp;
 
 	p.y = 0;
 	idx = 0;
-	if (!isvalid(tile, size) || (ntl = (t_tile*(ft_memalloc(sizeof(t_tile))))))
+	if (!isvalid(tile, size) || (ntl = (t_tile*)ft_memalloc(sizeof(t_tile))))
 		return (0);
 	while (p.y < 4)
 	{
 		p.x = 0;
 		while (p.x < 4)
 			if (tile[(p.y * 5) + p.x] == '#')
-				newtile.dots[idx] = p;
+			{
+				tmp = 0;
+				ntl->dots[idx++] = p;
+				while (tmp < idx - 2)
+					if (!hascontact(&(ntl->dots[idx - 1]), &(ntl->dots[tmp])))
+						return (0);
+			}
 	}
+	return (ntl);
 }
 
-int		read_tiles(int fd)
+//todo
+int				read_tiles(int fd)
 {
 	int		cnt;
 	char	buffer[TILE_SIZE];
@@ -71,8 +89,6 @@ int		read_tiles(int fd)
 	{
 		if ((cnt = read(fd, buffer, TILE_SIZE)) <= 0)
 			break ;
-		if (is_valid(buffer, cnt))
-			convert();
 		else
 			return (0);
 	}
